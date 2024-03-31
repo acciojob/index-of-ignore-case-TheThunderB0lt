a7e0efa8ca211cad4fb0281e62f5cf20a9f19dcc
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-03-31 20:17:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	let x = str.toLowerCase(s1);
+	let y = str.toLowerCase(s2);
+
+	return x.indexOf(y);
 }
 
 // Please do not change the code below
